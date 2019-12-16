69df4fb4d2217ab7e4137e053d088190a6d94ca9
Gil Megidish <gil@megidish.net>
2019-12-16 13:41:45 +0200

    Updates

--- app/src/main/java/com/testfairy/samples/sampleappwithfeedbackbutton/MainActivity.java
+++ app/src/main/java/com/testfairy/samples/sampleappwithfeedbackbutton/MainActivity.java
@@ -1,14 +1,11 @@
 package com.testfairy.samples.sampleappwithfeedbackbutton;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.testfairy.TestFairy;
 import com.testfairy.TestFairyFeedbackOverlay;
@@ -18,8 +15,7 @@ import java.util.TimerTask;
 
 public class MainActivity extends Activity {
 
-//	static private final String TESTFAIRY_APP_TOKEN = "APP-TOKEN";
-	static private final String TESTFAIRY_APP_TOKEN = "e27cf8c46bb25d8986e21915d700e493b268df0b";
+	static private final String TESTFAIRY_APP_TOKEN = "c0a3512fb696bfc899ded02d339f83fe4ab6aa18";
 
 	private ImageView imageView;
 
@@ -41,16 +37,9 @@ public class MainActivity extends Activity {
 		findViewById(R.id.report_a_bug_button).setOnClickListener(reportBugClickListener);
 		findViewById(R.id.take_screenshot_button).setOnClickListener(takeScreenshotClickListener);
 		findViewById(R.id.record_a_video_button).setOnClickListener(reproduceBugClickListener);
+		findViewById(R.id.record_a_video_with_overlay_button).setOnClickListener(reproduceBugWithOverlayClickListener);
 
 		findViewById(R.id.start_second_activity).setOnClickListener(startSecondActivityClickListener);
-	}
-
-	private void startRecording() {
-		// To record immediately, just call begin().
-		// TestFairy.begin(this, TESTFAIRY_APP_TOKEN);
-
-		// installOverlay() shows dialog to require user approval, and then calls begin().
-		TestFairyFeedbackOverlay.installOverlay(this, TESTFAIRY_APP_TOKEN, TestFairyFeedbackOverlay.OverlayPurpose.VIDEO);
 	}
 
 	private TimerTask rotateAndroidImageTask = new TimerTask() {
@@ -92,7 +81,16 @@ public class MainActivity extends Activity {
 	private View.OnClickListener reproduceBugClickListener = new View.OnClickListener() {
 		@Override
 		public void onClick(View view) {
-			startRecording();
+			Toast.makeText(MainActivity.this, "Started recording video", Toast.LENGTH_LONG).show();
+			TestFairy.begin(MainActivity.this, TESTFAIRY_APP_TOKEN);
+		}
+	};
+
+	private View.OnClickListener reproduceBugWithOverlayClickListener = new View.OnClickListener() {
+		@Override
+		public void onClick(View view) {
+			// installOverlay() shows dialog to require user approval, and then calls begin().
+			TestFairyFeedbackOverlay.installOverlay(MainActivity.this, TESTFAIRY_APP_TOKEN, TestFairyFeedbackOverlay.OverlayPurpose.VIDEO);
 		}
 	};
 }
